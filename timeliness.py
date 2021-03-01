d1fe6a932830c54d731220934c69ad5d47324399
hddesai <hdesai89@gmail.com>
2021-03-01 12:32:43 -0500

    Update timeliness.py

--- timeliness.py
+++ timeliness.py
@@ -4,5 +4,6 @@ import seaborn as sns
 df = pd.read_excel (r'X:\Users\hiral.desai\OneDrive - Association of Public Health Laboratories\Desktop\cah_analysis\Timeliness\interval_collection.xlsx')
 df.head(1)
 print (df)
-scat = sns.catplot(x="birth_year", y="initial_specimen_collection", kind ="box",data=df)
+box = sns.boxplot(x="birth_year", y="initial_specimen_collection",data=df, palette = "Set2")
+box.set(xlabel="Year", ylabel="Specimen Collection Time")
 plt.show()
